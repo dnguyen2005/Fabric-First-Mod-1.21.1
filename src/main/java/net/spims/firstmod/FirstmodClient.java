25cc963df073eff6ffe09a27bfe4119363c8ae9a
dnguyen2005 <davidnguyen6004@gmail.com>
2026-03-14 16:33:07 -0400

    Initial commit

---
+++ src/main/java/net/spims/firstmod/FirstmodClient.java
@@ -0,0 +1,10 @@
+package net.spims.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
